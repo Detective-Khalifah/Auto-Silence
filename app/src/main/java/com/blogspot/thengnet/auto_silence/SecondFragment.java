2ed9d8d3e1064e31146858455347745653d14617
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-07-02 06:49:59 +0100

    Configure {@link SecondFragment} to work with {@link SchedulesAdapter} in establishing a {@link MaterialCardView} that could be edited -- sole purpose of the {@link SecondFragment} is to edit {@link Schedule} {@link MaterialCardView}s; editing will be disabled in {@link FirstFragment} in subsequent commits. -Add 'descendantFocusability' attribute to root - {@link ConstraintLayout} - layout, so {@link TextInputEditText}s do not pop up the keyboard. -Re-add the intro text of hint {@link TextView} in {@link SecondFragment}'s fragment_second.xml
    Note: This is a working version of the app, due to chronological error in commiting resources like strings.xml some commits ago.

--- app/src/main/java/com/blogspot/thengnet/auto_silence/SecondFragment.java
+++ app/src/main/java/com/blogspot/thengnet/auto_silence/SecondFragment.java
@@ -5,21 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.blogspot.thengnet.auto_silence.databinding.FragmentSecondBinding;
+import com.blogspot.thengnet.auto_silence.databinding.ScheduleBinding;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-
-import com.blogspot.thengnet.auto_silence.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+
+    private Bundle mScheduleParams;
+    private ArrayList<String> mScheduleParamsList;
+
+    // for testing
+    private String[] currentSchedule;
 
     @Override
     public View onCreateView (
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
+        mScheduleParams = new Bundle();
+        currentSchedule = mScheduleParams.getStringArray("selected-schedule");
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -36,6 +50,26 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        // TEST
+        ScheduleBinding tryout = DataBindingUtil.inflate(getLayoutInflater(), R.layout.schedule, (ViewGroup) view.getParent(), false);
+        View fragmentRoot = binding.getRoot();
+        ((ConstraintLayout) fragmentRoot).addView(tryout.getRoot());
+        fragmentRoot.setPadding(16, 8, 16, 8);
+
+
+        // DUMMY TEST-EDIT DATA
+        SchedulesAdapter schedulesAdapter = new SchedulesAdapter(view.getContext(), 0,
+                new ArrayList<Schedule>());
+        tryout.setSchedule(new Schedule(false, "Edit it!",
+                "Schedule edit mode.", "2021-07-02", "06:30",
+                "2021-07-02", "15:00"));
+        tryout.executePendingBindings();
+
+        tryout.editStartDate.setOnClickListener(schedulesAdapter.new Dates(tryout.editStartDate, "date"));
+        tryout.editEndDate.setOnClickListener(schedulesAdapter.new Dates(tryout.editStartDate, "date"));
+        tryout.editStartDate.setOnClickListener(schedulesAdapter.new Dates(tryout.editStartDate, "date"));
+        tryout.editStartTime.setOnClickListener(schedulesAdapter.new Dates(tryout.editStartDate, "date"));
     }
 
     @Override
